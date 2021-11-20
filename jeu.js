d1decbeb32c9338bd319c2f1e886725238411cb6
Tristan <34968241+Torystan@users.noreply.github.com>
2021-11-20 18:06:57 +0100

    Ajout de la classe Entite et ajout de commentaires

--- jeu.js
+++ jeu.js
@@ -1,48 +1,58 @@
 //****************//
 //      class     //
 //****************//
-class Perso {
-    constructor() {
+class Entite {
+    constructor(unePosX, unePosY) {
         this.velX = 0;
         this.velY = 0;
-        this.posX = 300;
-        this.posY = canvas.height;
-        this.largeur = canvas.width/16;
-        this.hauteur = canvas.height/11;
+        this.posX = unePosX;
+        this.posY = unePosY;
+        this.largeur = canvas.width / 16;
+        this.hauteur = canvas.height / 11;
         this.vitesse = canvas.width * 0.0009;
         this.couleur = 'rgb(200, 50, 50)';
         this.surLeSol = true;
     }
 
-    frictionEtGravite(){
+    /**
+     * Augmente la velocité Y et deplace l'entité pour simuler la gravité.
+     * Baisse la vélocité X pour simuler la friction avec l'air et le sol.
+     */
+    frictionEtGravite() {
         this.velX *= friction;
         this.velY += gravite;
         this.posY += this.velY;
     }
 
-    saut(){
-        if(this.surLeSol){
-            this.velY = -10.2;
-            this.surLeSol = false;
-        }
-    }
-
-    deplacer(){
-        if(touches_appuye[touche.DROITE]) {
+    /**
+     * Déplace l'entitée dans toutes les direction à l'aide des touches du clavier.
+     * 
+     * - Aller à gauche : DROITE ou D
+     * - Aller à droite : GAUCHE ou Q
+     * - Aller vers le haut (sauter) : HAUT ou ESPACE ou Z
+     */
+    deplacer() {
+        if (touches_appuye[touche.DROITE] || touches_appuye[touche.D]) {
             this.velX += this.vitesse;
         }
-        if(touches_appuye[touche.GAUCHE]) {
+        if (touches_appuye[touche.GAUCHE] || touches_appuye[touche.Q]) {
             this.velX += -this.vitesse;
         }
-        if(touches_appuye[touche.HAUT]) {
+        if (touches_appuye[touche.HAUT] || touches_appuye[touche.ESPACE] || touches_appuye[touche.Z]) {
             this.saut();
         }
         this.posX += this.velX;
     }
 
-    verifierSolEtPlateforme(lesPlateformes){
-        if(this.posY > canvas.height - canvas.height/9.33){
-            this.posY = canvas.height - canvas.height/9.33;
+    /**
+     * Analyse si l'entité est entré en collision par le haut ou pas le bas,
+     * avec toutes les plateformes et avec le sol.
+     * 
+     * @param lesPlateformes liste de toutes les plateformes
+     */
+    verifierSolEtPlateforme(lesPlateformes) {
+        if (this.posY > canvas.height - canvas.height / 9.33) {
+            this.posY = canvas.height - canvas.height / 9.33;
             this.velY = 0;
             this.surLeSol = true;
         }
@@ -52,10 +62,15 @@ class Perso {
         });
     }
 
-    collisionPlateformeSol(unePlateforme){
-        if(this.velY > 0){
-            if(this.posY >= unePlateforme.posY - unePlateforme.hauteur && this.posY <= unePlateforme.posY){ //pieds dans la plateforme
-                if(this.posX + this.largeur >= unePlateforme.posX && this.posX <= unePlateforme.posX + unePlateforme.largeur){ //entre les bords d'une plateforme
+    /**
+     * Analyse si l'entité est entré en collision avec le haut d'une plateforme (en tombant dessus)
+     * 
+     * @param unePlateforme plateforme avec laquelle l'entité en entré en collision
+     */
+    collisionPlateformeSol(unePlateforme) {
+        if (this.velY > 0) {
+            if (this.posY >= unePlateforme.posY - unePlateforme.hauteur && this.posY <= unePlateforme.posY) { //pieds dans la plateforme
+                if (this.posX + this.largeur >= unePlateforme.posX && this.posX <= unePlateforme.posX + unePlateforme.largeur) { //entre les bords d'une plateforme
                     this.posY = unePlateforme.posY - unePlateforme.hauteur;
                     this.velY = 0;
                     this.surLeSol = true;
@@ -64,50 +79,87 @@ class Perso {
         }
     }
 
-    collisionPlateformePlafond(unePlateforme){
-        if(this.velY < 0){
-            if(this.posY - this.hauteur >= unePlateforme.posY - unePlateforme.hauteur && this.posY - this.hauteur <= unePlateforme.posY){ //tête dans la plateforme
-                if(this.posX + this.largeur >= unePlateforme.posX && this.posX <= unePlateforme.posX + unePlateforme.largeur){ //entre les bords d'une plateforme
-                    this.posY = unePlateforme.posY + this.hauteur + 1;
+    /**
+     * Analyse si l'entité est entré en collision avec le bas d'une plateforme (en sautant dedans)
+     * 
+     * @param unePlateforme plateforme avec laquelle l'entité en entré en collision
+     */
+    collisionPlateformePlafond(unePlateforme) {
+        if (this.velY < 0) {
+            if (this.posY - this.hauteur >= unePlateforme.posY - unePlateforme.hauteur && this.posY - this.hauteur <= unePlateforme.posY) { //tête dans la plateforme
+                if (this.posX + this.largeur >= unePlateforme.posX && this.posX <= unePlateforme.posX + unePlateforme.largeur) { //entre les bords d'une plateforme
                     this.velY = 0;
+                    this.posY = unePlateforme.posY + this.hauteur + 1;
                     this.declencherBosse(unePlateforme);
                 }
             }
         }
     }
 
-    declencherBosse(unePlateforme){
+    /**
+     * Déclenche la création de la bosse sur une plateforme
+     * 
+     * @param unePlateforme plateforme avec laquelle l'entité en entré en collision
+     */
+    declencherBosse(unePlateforme) {
         var valUneBosse = valBosse.CENTRE;
-        if(this.posX + this.largeur/2 < unePlateforme.posX + 30){
-            console.log("gauche");
+        if (this.posX + this.largeur / 2 < unePlateforme.posX + 30) {
             valUneBosse = valBosse.BORD_GAUCHE;
-        } else if(this.posX + this.largeur/2 > unePlateforme.posX + unePlateforme.largeur - 30){
-            console.log("droiche");
+        } else if (this.posX + this.largeur / 2 > unePlateforme.posX + unePlateforme.largeur - 30) {
             valUneBosse = valBosse.BORD_DROIT;
         }
-        var nouvelleBosse = new Bosse(this.posX + this.largeur/2, unePlateforme.posY, valUneBosse, unePlateforme.posX, unePlateforme.posX + unePlateforme.largeur);
+        var nouvelleBosse = new Bosse(this.posX + this.largeur / 2, unePlateforme.posY, valUneBosse, unePlateforme.posX, unePlateforme.posX + unePlateforme.largeur);
         unePlateforme.ajouterBosse(nouvelleBosse);
     }
 
-    teleportationBords(){
-        if(this.posX + this.largeur < 0){
+    /**
+     * Replace l'entitée lorsqu'elle dépace de l'écran pas les bords.
+     * - Si elle sort par la droite, elle réapparait à gauche
+     * - Si elle sort par la gauche, elle réapparait à droite
+     */
+    teleportationBords() {
+        if (this.posX + this.largeur < 0) {
             this.posX = canvas.width;
         }
-        if(this.posX > canvas.width){
+        if (this.posX > canvas.width) {
             this.posX = 0 - this.largeur;
         }
     }
 
-    mouvement(lesPlateformes){
+    /**
+     * Appel de toutes les fonctions necessaire au mouvement de l'entité.
+     * 
+     * @param lesPlateformes liste de toutes les plateformes
+     */
+    mouvement(lesPlateformes) {
         this.deplacer();
         this.frictionEtGravite();
         this.teleportationBords();
         this.verifierSolEtPlateforme(lesPlateformes);
     }
 
+    /**
+     * Dessine une entité.
+     */
     dessiner() {
         ctx.fillStyle = this.couleur;
         ctx.fillRect(this.posX, this.posY - this.hauteur, this.largeur, this.hauteur);
+    }
+}
+
+class Perso extends Entite {
+    constructor(unePosX, unePosY) {
+        super(unePosX, unePosY)
+    }
+
+    /**
+     * Modifie la vélocité Y pour faire sauter le personnage.
+     */
+    saut() {
+        if (this.surLeSol) {
+            this.velY = -10.5;
+            this.surLeSol = false;
+        }
     }
 }
 
@@ -116,25 +168,33 @@ class Plateforme {
         this.posX = unePosX;
         this.posY = unePosY;
         this.largeur = uneLargeur;
-        this.hauteur = canvas.height*0.03;
+        this.hauteur = canvas.height * 0.03;
         this.couleur = 'rgb(150, 150, 150)';
         this.bosses = [];
     }
 
-    ajouterBosse(uneBosse){
-        this.bosses.push([uneBosse, 30])//[<unBosse>, <timer>]
+    /**
+     * Ajoute une bosse à la plateforme.
+     * 
+     * @param uneBosse bosse à ajouter
+     */
+    ajouterBosse(uneBosse) {
+        this.bosses.push([uneBosse, 30])//[<uneBosse>, <timer>]
     }
 
+    /**
+     * Dessine la plateforme et ses éventuelles bosses.
+     */
     dessiner() {
         ctx.fillStyle = this.couleur;
         ctx.fillRect(this.posX, this.posY - this.hauteur, this.largeur, this.hauteur);
 
-        //Dessiner les bosses
+        //Dessiner les bosses.
         this.bosses.forEach(uneBosse => {
             uneBosse[0].dessiner();
             uneBosse[1] -= 1;
             //Si le timer est à 0
-            if(uneBosse[1] == 0){
+            if (uneBosse[1] == 0) {
                 this.bosses.splice(this.bosses.indexOf(uneBosse), 1);
             }
         });
@@ -146,55 +206,71 @@ class Bosse {
         this.posX = unePosX - 30;
         this.posY = unePosY;
         this.largeur = 60;
-        this.hauteur = canvas.height*0.03;
+        this.hauteur = canvas.height * 0.03;
         this.couleur = 'rgb(50, 50, 250)';
         this.decalerX(valPos, bordGauchePlat, bordDroitPlat);
     }
 
-    decalerX(val, leBordGauchePlat, leBordDroitPlat){
-        if(val === valBosse.BORD_GAUCHE){
-            //console.log("ok")
+    /**
+     * Decale la position de la bosse sur l'axe des abscisses et redéfini sa taille
+     * en fonction de sa position.
+     * 
+     * @param val valeur de la position (BORD_DROIT, BORD_GAUCHE ou CENTRE)
+     * @param leBordGauchePlat valeur sur l'axe des abscisses du bord gauche de la plateforme
+     * @param leBordDroitPlat valeur sur l'axe des abscisses du bord droit de la plateforme
+     */
+    decalerX(val, leBordGauchePlat, leBordDroitPlat) {
+        if (val === valBosse.BORD_GAUCHE) {
             this.posX = leBordGauchePlat;
             this.largeur = 30
-        } else if(val === valBosse.BORD_DROIT){
+        } else if (val === valBosse.BORD_DROIT) {
             this.posX = leBordDroitPlat - 30;
             this.largeur = 30
         }
     }
 
+    /**
+     * Dessine la bosse.
+     */
     dessiner() {
         ctx.fillStyle = this.couleur;
         ctx.fillRect(this.posX, this.posY - this.hauteur, this.largeur, this.hauteur);
     }
 }
 
-class Jeu{
-    init(){
+class Jeu {
+    init() {
         this.initPlateforme()
         window.addEventListener('keyup', keyHandler);
         window.addEventListener('keydown', keyHandler);
         window.setInterval(this.loop, 14);
     }
 
-    initPlateforme(){
+    /**
+     * Initialise toutes les plateformes.
+     */
+    initPlateforme() {
         //Première ligne
-        plateformes.push(new Plateforme(0, canvas.height - canvas.height/3.7, canvas.width/3))
-        plateformes.push(new Plateforme(canvas.width * 2/3, canvas.height - canvas.height/3.7, canvas.width/3))
+        plateformes.push(new Plateforme(0, canvas.height - canvas.height / 3.7, canvas.width / 3))
+        plateformes.push(new Plateforme(canvas.width * 2 / 3, canvas.height - canvas.height / 3.7, canvas.width / 3))
 
         //deuxième ligne
-        plateformes.push(new Plateforme(0, canvas.height - canvas.height/2.26, canvas.width/10))
-        plateformes.push(new Plateforme(canvas.width - canvas.width/10, canvas.height - canvas.height/2.26, canvas.width/10))
-        plateformes.push(new Plateforme((canvas.width - canvas.width/1.87)/2, canvas.height - canvas.height/2.12, canvas.width/1.87))
+        plateformes.push(new Plateforme(0, canvas.height - canvas.height / 2.26, canvas.width / 10))
+        plateformes.push(new Plateforme(canvas.width - canvas.width / 10, canvas.height - canvas.height / 2.26, canvas.width / 10))
+        plateformes.push(new Plateforme((canvas.width - canvas.width / 1.87) / 2, canvas.height - canvas.height / 2.12, canvas.width / 1.87))
 
         //troisième ligne
-        plateformes.push(new Plateforme(0, canvas.height - canvas.height/1.47, canvas.width/2.3))
-        plateformes.push(new Plateforme(canvas.width - canvas.width/2.3, canvas.height - canvas.height/1.47, canvas.width/2.3))
+        plateformes.push(new Plateforme(0, canvas.height - canvas.height / 1.47, canvas.width / 2.3))
+        plateformes.push(new Plateforme(canvas.width - canvas.width / 2.3, canvas.height - canvas.height / 1.47, canvas.width / 2.3))
     }
 
+    /**
+     * Boucle principal du jeu.
+     */
     loop() {
         ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.fillRect(0, 0, canvas.width, canvas.height);
-    
+
         plateformes.forEach(plat => {
             plat.dessiner();
         });
@@ -212,14 +288,18 @@ var ctx = canvas.getContext('2d');
 const touches_appuye = [];
 const friction = 0.75;
 const gravite = 0.5;
-const perso = new Perso();
+const perso = new Perso(canvas.width / 2, canvas.height * 0.9);
 const plateformes = [];
 
 //énumération des touches
 const touche = {
     DROITE: 39,
+    D: 68,
     GAUCHE: 37,
+    Q: 81,
     HAUT: 38,
+    ESPACE: 32,
+    Z: 90,
 }
 
 //énumération valeurs d'une bosse
